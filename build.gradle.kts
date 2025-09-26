75469d4424731c66577df2fe7a6957d17846f701
Dominic Wienzek <dominic.wienzek@gmail.com>
2025-09-26 19:54:17 +0200

    Preload shared kotlin gradle plugins for single classloader instance

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
+  // Preload shared Kotlin Gradle plugins once via version catalog to ensure a single classloader instance
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.plugin.serialization) apply false
+  alias(libs.plugins.kotlin.plugin.spring) apply false
+
   alias(libs.plugins.versions)
 }
